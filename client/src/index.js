0bdc9a427961048b34cd493f00583bf44c3de069
R-Morpheus <gagach16rus@gmail.com>
2022-11-09 16:04:33 +0300

    fix old version react

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import {createRoot} from 'react-dom/client'
 import {store} from "./reducers";
 import {Provider} from "react-redux";
+import App from "./components/App";
 
 
-ReactDOM.render(
+
+const root = createRoot(document.getElementById('root'));
+root.render(
     <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+        <App/>,
+    </Provider>
 );
